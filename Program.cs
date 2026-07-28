1873442fa31aad0c0935e2806019a080285abf68
César Chadburn (Phenimation) <192460210+Phenimation@users.noreply.github.com>
2026-07-28 16:00:24 +0200

    Civilia:V0.1.1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string civilisationName = "";
+
+Game();
+
+void Game()
+{
+    civilisationName = SelectCivilisationName();
+}
+
+string SelectCivilisationName()
+{
+    string nameChoosed = "";
+
+    Console.Write("Choissisez le nom de votre Civilisation : ");
+    nameChoosed = Console.ReadLine() ?? string.Empty;
+
+    return nameChoosed;
+}
